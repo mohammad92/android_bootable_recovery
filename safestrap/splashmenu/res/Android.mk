f8dba86cd404ec3fa7e7c8e7760f9e32e71e4fc6
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2022-05-20 07:03:39 +0300

    splashmenu: Fix res generation

--- safestrap/splashmenu/res/Android.mk
+++ safestrap/splashmenu/res/Android.mk
@@ -1,19 +1,17 @@
 LOCAL_PATH := $(call my-dir)
 
+SS_RES := $(LOCAL_PATH)/$(DEVICE_RESOLUTION)
+
+SS_RES_CMD := \
+	mkdir -p $(PRODUCT_OUT)/install-files/$(SS_LOC)/safestrap/res && \
+	cp -fr $(SS_RES)/* $(PRODUCT_OUT)/install-files/$(SS_LOC)/safestrap/res/
+
 # Transfer in the resources for the device
 include $(CLEAR_VARS)
 LOCAL_MODULE := ssres
 LOCAL_MODULE_TAGS := eng
 LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
-LOCAL_MODULE_PATH := $(TARGET_RECOVERY_OUT)/../install-files/$(SS_LOC)/safestrap/res
-
-SS_RES := $(LOCAL_PATH)/$(DEVICE_RESOLUTION)
-
-SS_RES_GEN := $(intermediates)/ssres
-$(SS_RES_GEN):
-	mkdir -p $(PRODUCT_OUT)/install-files/$(SS_LOC)/safestrap/res
-	cp -fr $(SS_RES)/* $(PRODUCT_OUT)/install-files/$(SS_LOC)/safestrap/res/
-
-LOCAL_GENERATED_SOURCES := $(SS_RES_GEN)
-LOCAL_SRC_FILES := ssres $(SS_RES_GEN)
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_POST_INSTALL_CMD := $(SS_RES_CMD)
 include $(BUILD_PREBUILT)
